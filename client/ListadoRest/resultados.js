7b38f254bc7d6754418a30e05b2dfb1b52a9db87
kopolo435 <samirfanilla@gmail.com>
2023-10-18 07:15:25 -0500

    Agrega funcion para agregar resutarantes del json

--- client/ListadoRest/resultados.js
+++ client/ListadoRest/resultados.js
@@ -1,3 +1,11 @@
 import listaRestaurantes from './listaRestaurantes.json' assert { type: 'json' };
-
+import createCard  from './cardCreator.js';
 const resultados = document.getElementById("results");
+
+function fillResults(){
+    listaRestaurantes.forEach( restaurante =>{
+        resultados.appendChild(createCard(restaurante));
+    })
+}
+
+fillResults();
