ba1e6b3ee01880c8f76783f80ebd134e479e2815
kopolo435 <samirfanilla@gmail.com>
2023-10-18 12:59:25 -0500

    Agrega declaracion de elementos de info restaurante

---
+++ client/InfoRest/restaurante.js
@@ -0,0 +1,5 @@
+const tituloRestaurante = document.getElementById("titulo");
+const descripcionRestaurante = document.getElementById("descripcion");
+const ubicacionRestaurante = document.getElementById("ubicacion");
+const caracteristicasRestaurante = document.getElementById("caracteristicas");
+const contactoRestaurante = document.getElementById("contacto");
