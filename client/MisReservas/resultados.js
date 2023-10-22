bcc6c2391f9da06cc7e0f905ef5b9b1be9e2d943
kopolo435 <samirfanilla@gmail.com>
2023-10-22 09:34:37 -0500

    Adapta codigo para funcionar con reservas

--- client/MisReservas/resultados.js
+++ client/MisReservas/resultados.js
@@ -1,10 +1,10 @@
-import listaRestaurantes from './listaRestaurantes.json' assert { type: 'json' };
+import listaReservas from './listaReservas.json' assert { type: 'json' };
 import createCard  from './cardCreator.js';
 const resultados = document.getElementById("results");
 
 function fillResults(){
-    listaRestaurantes.forEach( restaurante =>{
-        resultados.appendChild(createCard(restaurante));
+    listaReservas.forEach( reserva =>{
+        resultados.appendChild(createCard(reserva));
     })
 }
 
