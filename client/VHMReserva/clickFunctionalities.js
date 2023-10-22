58d3821caff04044587b6c52c7b577ffdedb0498
Shruti Tewaney <shruti072003@gmail.com>
2023-10-22 00:52:25 -0500

    Added modal functionality

--- client/VHMReserva/clickFunctionalities.js
+++
@@ -1,7 +0,0 @@
-const calendarIcon = document.querySelector('.fa-regular.fa-calendar-days.fa-lg.calendar-icon');
-const dateInput = document.querySelector('#dateInput');
-
-calendarIcon.addEventListener('click', () => {
-    $(dateInput).datepicker(); // Initialize the datepicker
-    $(dateInput).datepicker('show');
-  });
