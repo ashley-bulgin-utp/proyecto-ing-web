f98396fd877f0ed8bda05179f0a0a2d7f781d866
kopolo435 <samirfanilla@gmail.com>
2023-11-18 19:41:30 -0500

    Agrega obtencion de parametro y sus valores
    Se implementa una funcion la cual obtiene el name y valor de la url al
    escoger los filtros. Se guarda la informacion en un array que tiene
    por keys el name de los valores y su value es un string o si se repite
    el nombre en el query entonces el value del name es un array con los
    distintos valores. La funcion retorna este array

--- app/controllers/ListadoRest.php
+++ app/controllers/ListadoRest.php
@@ -4,12 +4,57 @@
         public function __construct() {
         }
 
-        public function resultados() {
+        public function resultados($array = []) {
             $data = [
                 'users',
             ];
-
+            $parameters = $this->getParameters();
+            // Output the parameters with their values
+            foreach ($parameters as $key => $value) {
+                if (is_array($value)) {
+                    foreach ($value as $val) {
+                        echo $key . ': ' . $val . '<br>';
+                    }
+                } else {
+                    echo $key . ': ' . $value . '<br>';
+                }
+            }
             $this->view('resultados', $data);
+        }
+
+        private function getParameters(){
+            $url = $_SERVER['REQUEST_URI'];
+            // Extrae la query de la URL
+            $queryString = parse_url($url, PHP_URL_QUERY);
+            
+            // Separa la string en los &
+            $parameterPairs = explode('&', $queryString);
+            
+            // inicializa variable que contiene los parametros
+            $parameters = [];
+            
+            foreach ($parameterPairs as $pair) {
+                //Divide los parametros a nombre y valor
+                list($name, $value) = explode('=', $pair, 2);
+                
+                // URL decode the values
+                $value = urldecode($value);
+                $name = urldecode(($name));
+                // Comprueba si ya existe el parametro en el array
+                if (array_key_exists($name, $parameters)) {
+                    // Si el parametro ya existe, lo convierte a un array
+                    if (!is_array($parameters[$name])) {
+                        $parameters[$name] = [$parameters[$name]];
+                    }
+                    // Agrega el nuevo valor al array del parametro
+                    $parameters[$name][] = $value;
+                } else {
+                    // Si el parametro no existe, lo agrega al array
+                    $parameters[$name] = $value;
+                }
+            }
+
+            return $parameters;
         }
         
     }
