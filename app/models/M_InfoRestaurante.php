2ce472da86cadbaa64cadba9368cc76e63ed6542
kopolo435 <samirfanilla@gmail.com>
2023-11-19 15:46:19 -0500

    Modifica consulta para incluir descripcion de rest

--- app/models/M_InfoRestaurante.php
+++ app/models/M_InfoRestaurante.php
@@ -28,6 +28,7 @@
                 r.res_tipoRes,
                 r.res_resena,
                 r.res_ubicacion,
+                descrip.desc_desc,
                 GROUP_CONCAT(tf.tip_nombre SEPARATOR ', ') AS facilidades,
                 CONCAT(hd.hor_dia, ': ', GROUP_CONCAT(CONCAT(d.dis_hor_inicio, ' to ', d.dis_hor_cierre) SEPARATOR ', ')) AS dias_con_horas
             FROM 
@@ -40,6 +41,8 @@
                 rest_facilidades rf ON r.res_id = rf.rf_res_id
             LEFT JOIN 
                 tipo_facilidades tf ON rf.rf_tip_id = tf.tip_id
+            LEFT JOIN
+                desc_res descrip ON r.res_desc_id = descrip.desc_id
             WHERE 
                 r.res_id = $id;
             ";
