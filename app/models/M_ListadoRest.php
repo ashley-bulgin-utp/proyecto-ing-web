33238a29cde50cf024a9c4726801667c2f71f6a9
kopolo435 <samirfanilla@gmail.com>
2023-11-18 19:50:53 -0500

    Agrega estructura inicial del modelo de ListadoRest

---
+++ app/models/M_ListadoRest.php
@@ -0,0 +1,13 @@
+<?php
+    class M_ListadoRest {
+        private $db;
+
+        public function __construct() {
+            $this->db = new Database();
+        }
+
+        public function getRestaurantes() {
+
+        }
+    }
+?>
