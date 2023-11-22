a21619540255d5fd5d6ff7c49f70ee68e065ae77
Shruti Tewaney <shruti072003@gmail.com>
2023-11-22 12:36:54 -0500

    Configured user profile upload

--- app/controllers/AjusteUsuario.php
+++ app/controllers/AjusteUsuario.php
@@ -1,12 +1,43 @@
 <?php
-    class AjusteUsuario extends Controller {
-        public function __construct() {
+class AjusteUsuario extends Controller
+{
+
+    private $userModel;
+
+    public function __construct()
+    {
+    }
+
+    public function ajuste($data)
+    {
+        $userID = $_SESSION['user_id'];
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Profile Pic
+            $data = [
+                'image' => trim($_POST['image']),
+
+                'image_err' => ''
+            ];
+
+            $targetDir = "../../public/assets/ProfilePics";
+            $fileName = basename($_FILES["image"]["name"]);
+            $uniqeuFileName = $userID . "_" . $fileName;
+            $targetFilePath = $targetDir . $uniqeuFileName;
+
+            // Verificar tamaño del archivo
+            if ($_FILES['image']['size'] > 5000000) {
+                $data['image_err'] = 'El archivo supera el limite';
+                var_dump('El archivo supera el limite');
+            } else {
+            }
+
             
         }
 
-        public function ajuste($name) {
-            $this->view('ajusteUsuario');
-        }
+        $this->view('ajusteUsuario');
+
+
         
     }
-?>
+}
--- app/views/AjusteUsuario.php
+++ app/views/AjusteUsuario.php
@@ -16,12 +16,13 @@
         <h1 class="mobileh1">Ajustes de Perfil</h1>
         <div id="ajustar-perfil">
                 <div class="perfil">
-                    <img src="<?php echo URLROOT; ?>/assets/AjusteUsuario/placeholderProfile.png" alt="Foto de perfil">
+                    <img class="profile_img" src="<?php echo URLROOT; ?>/assets/AjusteUsuario/placeholderProfile.png" alt="Foto de perfil">
                 </div>
                 <div class="upload">
-                    <form action="" class="ajustarPerfil">
+                    <form action="<?php echo URLROOT; ?>/AjusteUsuario/ajuste"<?php echo $_SESSION['user_id'] ?> class="ajustarPerfil" method="POST" enctype="multipart/form-data">
                         <label for="imageUpload">Subir Archivo | <i class="fa-solid fa-cloud-arrow-up"></i></label>
-                        <input type="file" id="imageUpload" name="image" accept=".jpeg, .jpg, .png" onchange="checkFile(this)" required>
+                        <input type="file" id="imageUpload" name="image" accept=".jpeg, .jpg, .png" value="<?php echo $data['image'] ?>" required>
+                        <p style="color: red;"><?php echo $data['image_err'] ?></p>
                         <p>Archivos aceptados: (.jpeg, .jpg, .png, max 5MB)</p>
                         <p id="fileSizeError" style="color: red; display: none;">File size exceeds the limit (5MB).</p>
                     </form>
@@ -44,14 +45,14 @@
                 </div>
                 <div class="field-group">
                     <label for="telephone">Teléfono</label>
-                    <input type="tel" pattern="[0-9]" id="telephone" required>
+                    <input type="tel" id="telephone" required>
                 </div>
                 <div class="field-group">
                     <label for="password">Contraseña</label>
                     <input type="password" id="password" required>
                 </div>
                 <div class="botones">
-                    <button type="submit" class="btn-cancelar">Cancelar</button>
+                    <button type="submit" class="btn-cancelar" onclick="this.form.reset();">Cancelar</button>
                     <button type="submit" class="btn-guardar">Guardar</button>
                 </div>
             </form>
