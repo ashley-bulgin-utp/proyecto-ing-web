264dd526fca5c477c504fc557f578a2415974710
Shruti Tewaney <shruti072003@gmail.com>
2023-11-26 21:22:41 -0500

    Structured Register logic to POO

--- app/controllers/Register.php
+++ app/controllers/Register.php
@@ -34,53 +34,7 @@
                     'regphone_err' => ''
                 ];
 
-                // Validar nombre
-                if(empty($data['regname'])) {
-                    $data['regname_err'] = 'Por favor ingrese su nombre';
-                }
-
-                // Validar apellido
-                if(empty($data['regapellido'])) {
-                    $data['regapellido_err'] = 'Por favor ingrese su apellido';
-                }
-
-                // Validar contraseña
-                if(empty($data['regpass'])) {
-                    $data['regpass_err'] = 'Por favor ingrese su contraseña';
-                } elseif(strlen($data['regpass']) < 6) {
-                    $data['regpass_err'] = 'La contraseña debe tener al menos 6 caracteres';
-                }
-
-                // Validar correo
-                if(empty($data['regmail'])) {
-                    $data['regmail_err'] = 'Por favor ingrese su correo electrónico';
-                } else {
-                    // Validar si el correo ya existe
-                    if($this->userModel->findUserByEmail($data['regmail'])) {
-                        $data['regmail_err'] = 'El correo ya existe';
-                    }
-                }
-
-                // Validar teléfono
-                if(empty($data['regphone'])) {
-                    $data['regphone_err'] = 'Por favor ingrese su teléfono';
-                }
-
-                // Asegurarse de que no haya errores
-                if (empty($data['regname_err']) && empty($data['regapellido_err']) && empty($data['regmail_err']) && empty($data['regphone_err']) && empty($data['regpass_err'])) {
-                    // Encripcion del password
-                    $data['regpass'] = password_hash($data['regpass'], PASSWORD_DEFAULT);
-
-                    // Registrar usuario
-                    if($this->userModel->registrarUsuario($data)) {
-                        redirect('Login/login/1');
-                    } else {
-                        die('Algo salió mal');
-                    }
-                } else {
-                    // Cargar vista con errores
-                    $this->view('register', $data);
-                }
+                $this->checkParams($data);
 
             } else {
                 // Mostrar formulario de registro
@@ -100,6 +54,57 @@
                 ];
                 // Cargar vista
                 $this->view('register', $postData);
+            }
+        }
+
+        // Validar parametros
+        public function checkParams($data) {
+            // Validar nombre
+            if(empty($data['regname'])) {
+                $data['regname_err'] = 'Por favor ingrese su nombre';
+            }
+
+            // Validar apellido
+            if(empty($data['regapellido'])) {
+                $data['regapellido_err'] = 'Por favor ingrese su apellido';
+            }
+
+            // Validar contraseña
+            if(empty($data['regpass'])) {
+                $data['regpass_err'] = 'Por favor ingrese su contraseña';
+            } elseif(strlen($data['regpass']) < 6) {
+                $data['regpass_err'] = 'La contraseña debe tener al menos 6 caracteres';
+            }
+
+            // Validar correo
+            if(empty($data['regmail'])) {
+                $data['regmail_err'] = 'Por favor ingrese su correo electrónico';
+            } else {
+                // Validar si el correo ya existe
+                if($this->userModel->findUserByEmail($data['regmail'])) {
+                    $data['regmail_err'] = 'El correo ya existe';
+                }
+            }
+
+            // Validar teléfono
+            if(empty($data['regphone'])) {
+                $data['regphone_err'] = 'Por favor ingrese su teléfono';
+            }
+
+            // Asegurarse de que no haya errores
+            if (empty($data['regname_err']) && empty($data['regapellido_err']) && empty($data['regmail_err']) && empty($data['regphone_err']) && empty($data['regpass_err'])) {
+                // Encripcion del password
+                $data['regpass'] = password_hash($data['regpass'], PASSWORD_DEFAULT);
+
+                // Registrar usuario
+                if($this->userModel->registrarUsuario($data)) {
+                    redirect('Login/login/1');
+                } else {
+                    die('Algo salió mal');
+                }
+            } else {
+                // Cargar vista con errores
+                $this->view('register', $data);
             }
         }
         
