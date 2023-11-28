3dd608ffa2d50144306ac66a0ad1ac0a5442c30d
Shruti Tewaney <shruti072003@gmail.com>
2023-11-28 10:39:55 -0500

    Added update functionality

--- app/controllers/ModificarReserva.php
+++ app/controllers/ModificarReserva.php
@@ -1,16 +1,112 @@
 <?php
     class ModificarReserva extends Controller {
         
+        private $restModel;
         private $reservationModel;
+        private $response = '';
+        private $error = '';
 
         public function __construct() {
+            $this->restModel = $this->model('M_InfoRestaurante');
             $this->reservationModel = $this->model('M_Reservas');
         }
 
         public function modificar($id) {
-            $reservaID = $id;
+            global $response, $error;
+            $reservID = $id;
+            $userID = $_SESSION['user_id'];
 
-            $this->view('ModificarReserva');
+            // Fetch info del restaurante
+            $restInfo = $this->reservationModel->getReservationsByUser($userID);
+            if ($restInfo) {
+                $restData = [
+                    'id' => $restInfo->res_id,
+                    'nombre' => $restInfo->res_nombre,
+                    'imagen1' => $restInfo->res_imagen1,
+                    'imagen2' => $restInfo->res_imagen2,
+                    'imagen3' => $restInfo->res_imagen3,
+                    'horario' => $restInfo -> dias_con_horas,
+                    'reserva_id' => $id
+                ];
+            };
+            
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $reservaData = [
+                    'dia' => trim($_POST['dia']),
+                    'sillasBebe' => trim($_POST['sillasBebe']),
+                    'hora' => trim($_POST['time']),
+                    'comentarios' => trim($_POST['comentario']),
+                    'personas' => trim($_POST['personas']),
+                ];
+
+                // Validar parametros de reserva
+                if($this->checkParams($reservaData, $restData)) {
+                    // Enviar a la bd
+                    if($this->reservationModel->updateReservation($reservID, $reservaData)) {
+                        $response = 'Se ha modificado su reserva!';
+                    } else {
+                        $error = 'Se ha producido un error, por favor intente nuevamente.';
+                    }
+                    
+                } else {
+                    $error = 'El restaurante no esta disponsible para la hora seleccionada.';
+                }
+            } else {
+                $reservaData = [
+                    'dia' => '',
+                    'sillasBebe' => '',
+                    'hora' => '',
+                    'comentarios' => '',
+                    'personas' => '',
+                ];
+            }
+
+
+            $this->view('ModificarReserva', ['restData' => $restData, 'reservaData' => $reservaData, 'response' => $response, 'error' => $error]);
+              
+        }
+
+        // Verificar parametros de la reserva
+        public function checkParams($reservaData, $restData) {
+            // Verificar campo comentarios
+            if(empty($reservaData['comentarios'])) {
+                $reservaData['comentarios'] = '';
+            }            
+            // Verificar campo hora
+            if($reservaData['hora']) {
+                global $response, $error;
+                // Verificar horario de restaurante segun dia seleccionado
+                $day = $this->getWeekDay($reservaData);
+                $horario = $this->restModel->getRestTime($restData['id'], $day);
+
+                // Cambiar formato de las horas
+                $horaSeleccionada = new DateTime($reservaData['hora']);
+                $horaInicio = new DateTime($horario->dis_hor_inicio);
+                $horaCierre = new DateTime($horario->dis_hor_cierre);
+
+                if ($horaSeleccionada >= $horaInicio && $horaSeleccionada <= $horaCierre) {
+                    return true;
+                                        
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        // Funcion que retorno dia de la semana
+        public function getWeekDay($reservaData) {
+            $dateTime = new DateTime($reservaData['dia']);
+            $weekDay = $dateTime->format('N');
+
+            if ($weekDay >= 1 && $weekDay <= 5) {
+                $day = 1;
+            } elseif ($weekDay == 6) {
+                $day = 2;
+            } elseif ($weekDay == 7) {
+                $day = 3;
+            }
+
+            return $day;
         }
     }
 ?>
--- app/views/ModificarReserva.php
+++ app/views/ModificarReserva.php
@@ -1,8 +1,9 @@
-<?php require APPROOT.'/views/includes/components/Menu.php';
+<?php require APPROOT . '/views/includes/components/Menu.php';
     $restData = $data['restData'];
     $reservaData = $data['reservaData'];
 ?>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,53 +12,54 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.13.2/themes/smoothness/jquery-ui.css">
 </head>
+
 <body>
     <div id="mainContent">
         <div class="upper">
-            <a href="<?php echo URLROOT; ?>/MisReservas/misReservas/<?php echo $_SESSION['user_id'] ?>"><i class="fa-solid fa-arrow-left left-arrow"></i></a>
+            <a href="<?php echo URLROOT; ?>/VerReserva/ver/<?php echo $restData['id'] ?>"><i class="fa-solid fa-arrow-left left-arrow"></i></a>
             <h1><?php echo $restData['nombre'] ?></h1>
         </div>
         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-indicators">
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
             <div class="carousel-inner">
-              <div class="carousel-item active c-item">
-                <img src="<?php echo $restData['imagen1'] ?>" class="d-block  c-img" alt="...">
-              </div>
-              <div class="carousel-item c-item">
-                <img src="<?php echo $restData['imagen2'] ?>" class="d-block  c-img" alt="...">
-              </div>
-              <div class="carousel-item c-item">
-                <img src="<?php echo $restData['imagen3'] ?>" class="d-block  c-img" alt="...">
-              </div>
+                <div class="carousel-item active c-item">
+                    <img src="<?php echo $restData['imagen1'] ?>" class="d-block  c-img" alt="imagen-restaurante">
+                </div>
+                <div class="carousel-item c-item">
+                    <img src="<?php echo $restData['imagen2'] ?>" class="d-block  c-img" alt="imagen-restaurante">
+                </div>
+                <div class="carousel-item c-item">
+                    <img src="<?php echo $restData['imagen3'] ?>" class="d-block  c-img" alt="imagen-restaurante">
+                </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
             </button>
             <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
             </button>
         </div>
-        <div id="reservaForm" method="post">
-            <form action="">
+        <div id="reservaForm">
+            <form id="reservar" action="<?php echo URLROOT; ?>/ModificarReserva/modificar/<?php echo $restData['reserva_id'] ?>" method="POST">
                 <!-- Fecha -->
                 <div class="field-group">
                     <label for="dateInput" class="sr-only">Fecha</label>
-                    <input type="date" name="date" id="dateInput" value="<?php echo $reservaData['dia'] ?>" required>
+                    <input type="date" name="dia" id="dateInput" min="<?php echo date('Y-m-d'); ?>" value="<?php echo $reservaData['dia'] ?>" required>
                 </div>
                 <!-- Sillas de bebe -->
                 <div class="field-group">
-                    <label for="sillas-dd" class="sr-only">Sillas</label>
-                    <select id='sillas-dd' name='number' required>
-                      <option value='0' <?php echo ($reservaData['sillasBebe'] == 0) ? "selected = 'selected'" : ''; ?>>No requerido</option>
-                      <option value='1' <?php echo ($reservaData['sillasBebe'] == 1) ? "selected = 'selected'" : ''; ?>>1 silla bebe</option>
-                      <option value='2' <?php echo ($reservaData['sillasBebe'] == 2) ? "selected = 'selected'" : ''; ?>>2 sillas bebe</option>
-                      <option value='3' <?php echo ($reservaData['sillasBebe'] == 3) ? "selected = 'selected'" : ''; ?>>3 sillas bebe</option>
+                    <label for="sillas-dd" class="sr-only">Fecha</label>
+                    <select id='sillas-dd' name='sillasBebe' required>
+                        <option value='0' selected="selected">No requerido</option>
+                        <option value='1'>1 silla bebe</option>
+                        <option value='2'>2 sillas bebe</option>
+                        <option value='3'>3 sillas bebe</option>
                     </select>
                 </div>
                 <!-- Hora -->
@@ -68,15 +70,15 @@
                 <!-- Comentario -->
                 <div class="field-group-textarea">
                     <label for="comentario">Comentarios adicionales</label>
-                    <textarea name="comentario" id="comentario" placeholder="<?php echo($reservaData['comentarios']) ?>" ></textarea>
+                    <textarea name="comentario" id="comentario"></textarea>
                 </div>
                 <div class="field-group">
                     <label for="personas-dd" class="sr-only">Personas</label>
-                    <select id='personas-dd' name='number' required >
-                      <option value='1' <?php echo ($reservaData['personas'] == 1) ? "selected = 'selected'" : ''; ?>>1 persona</option>
-                      <option value='2'  <?php echo ($reservaData['personas'] == 2) ? "selected = 'selected'" : ''; ?>'>2 personas</option>
-                      <option value='3' <?php echo ($reservaData['personas'] == 3) ? "selected = 'selected'" : ''; ?>>3 personas</option>
-                      <option value='4' <?php echo ($reservaData['personas'] == 4) ? "selected = 'selected'" : ''; ?>>4+ personas</option>
+                    <select id='personas-dd' name='personas' required>
+                        <option value='1' selected="selected">1 persona</option>
+                        <option value='2'>2 personas</option>
+                        <option value='3'>3 personas</option>
+                        <option value='4'>4+ personas</option>
                     </select>
                 </div>
             </form>
@@ -100,14 +102,15 @@
                 }
                 
                 if (!empty($data['response'])) {
-                    echo "<button type='submit' form='reservar' class='btn btn-primary btn-reservar' disabled>Reservar</button>";
+                    echo "<button type='submit' form='reservar' class='btn btn-primary btn-reservar' disabled>Modificar</button>";
                 } else {
-                    echo "<button type='submit' form='reservar' class='btn btn-primary btn-reservar'>Reservar</button>";
+                    echo "<button type='submit' form='reservar' class='btn btn-primary btn-reservar'>Modificar</button>";
                 }
             ?>
         </div>
 
     </div>
 </body>
+
 </html>
-<?php require APPROOT.'/views/includes/components/Footer.php' ?>
+<?php require APPROOT . '/views/includes/components/Footer.php' ?>
