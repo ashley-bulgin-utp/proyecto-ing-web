a57cdb12cff5c172992496446814799af289406f
kopolo435 <samirfanilla@gmail.com>
2023-12-01 11:38:53 -0500

    Agrega redirect cuando se realiza un search

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -5,6 +5,11 @@
         }
 
         public function home($name) {
+            $url = $_SERVER['REQUEST_URI'];
+            $queryString = parse_url($url, PHP_URL_QUERY);
+            if($queryString){
+                header('Location: http://localhost/proyecto-ing-web/public/ListadoRest/resultados/1?'.$queryString);
+            }
             $this->view('home');
         }
         
